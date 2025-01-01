f1f0c39a4cd97c6121b05af25227a61d1e9cc259
0xJulian <nightbot1144@gmail.com>
2025-01-01 01:45:41 +0100

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '0xjulian'
+description 'Gangwar system non framework dependend'
+version '1.0.0'
+lua54 'yes'
+ui_page 'nui/index.html'
+
+client_scripts {
+    'config/cfg_client.lua',
+    'src/client/*.lua'
+}
+
+server_scripts {
+    'config/cfg_server.lua',
+    'src/server/*.lua'
+}
+
+shared_scripts {
+    'config/cfg_shared.lua'
+}
+
+files {
+    'nui/*.html',
+    'nui/*.js',
+    'nui/*.css'
+}
